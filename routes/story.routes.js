b7ccaf37eeb7d51ba16a7c0b09dc0ff41c0a9106
Marianne Agulhon <marianne.agulhon@gmail.com>
2023-02-21 18:20:11 +0100

    Create the route get one story

--- routes/story.routes.js
+++ routes/story.routes.js
@@ -1,18 +1,21 @@
-const router = require('express').Router()
-const User = require('../models/User.model')
-const Story = require('../models/Story.model')
-const Chapter = require('../models/Chapter.model')
-const mongoose = require('mongoose')
-const isAuthenticated = require('./../middlewares/isAuthenticated')
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Story = require("../models/Story.model");
+const Chapter = require("../models/Chapter.model");
+const mongoose = require("mongoose");
+const isAuthenticated = require("./../middlewares/isAuthenticated");
 
+router.get("/stories", async (req, res, next) => {
+  const finishedStories = await Story.find({ chapterCount: 0 }).populate(
+    "chapters"
+  );
+  const unfinishedStories = await Story.find({
+    chapterCount: { $gt: 0 },
+  }).populate("chapters");
 
-router.get('/stories', async (req, res, next) => {
-  const finishedStories = await Story.find({chapterCount: 0}).populate('chapters')
-  const unfinishedStories = await Story.find({chapterCount: {$gt: 0}}).populate('chapters')
-  
-  console.log({ finishedStories, unfinishedStories })
-  res.render('stories', { finishedStories, unfinishedStories })
-})
+  console.log({ finishedStories, unfinishedStories });
+  res.render("stories", { finishedStories, unfinishedStories });
+});
 
 // router.get('/stories', async (req, res, next) => {
 //   const finishedStories = []
@@ -37,45 +40,52 @@ router.get('/stories', async (req, res, next) => {
 //   res.render('stories', { finishedStories, toContinue })
 // })
 
-  // for (const oneStory of stories) {
-  //   const chapters = await Chapter.find({story: oneStory._id})
-  //   if (chapters.length === 5) {
-  //     finishedStories.push()
-  //   }
-  // }
+// for (const oneStory of stories) {
+//   const chapters = await Chapter.find({story: oneStory._id})
+//   if (chapters.length === 5) {
+//     finishedStories.push()
+//   }
+// }
 
-
-router.post('/stories', async (req, res, next) => {
+router.post("/stories", async (req, res, next) => {
   try {
     const chapter = await Chapter.create({
       author: req.session.currentUser._id,
       // story: story._id,
       content: req.body.story,
-    })
+    });
     const story = await Story.create({
       author: req.session.currentUser._id,
       title: req.body.title,
       chapters: [chapter._id],
-    })
-    console.log(req.body)
-    res.redirect('stories')
+    });
+    console.log(req.body);
+    res.redirect("stories");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-router.get('/stories/:StoryId', async (req, res, next) => {
-  const oneStory = await Story.find({
-    title: req.body.title,
-    author: req.body.author,
-    chapters: req.body.chapters
-  })
-  res.render('oneStory')
-})
+router.get("/stories/:storyId", async (req, res, next) => {
+  console.log('here');
+  try {
+    const oneStory = await Story.findById(req.params.storyId).populate('chapters');
+    res.json(oneStory)
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/stories/:StoryId', (req, res, next) => {
-  
-  res.render('oneStory')
-})
+// router.post('/stories/:storyId', (req, res, next) => {
+//   // const editStory =
+//   // const addChapter =
+//   res.render('oneStory')
+// })
 
-module.exports = router
+
+// router.post('/stories/:storyId/delete', (req, res, next) => {
+//   // const deleteStory =
+//   res.render('oneStory')
+// })
+
+module.exports = router;
